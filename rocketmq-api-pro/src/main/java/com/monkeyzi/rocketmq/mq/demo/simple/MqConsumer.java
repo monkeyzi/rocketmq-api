80ecc7d310f348ce397f74b90aaea803e0997d55
854152531@qq.com <854152531@qq.com>
2019-08-18 22:45:24 +0800

    修改

--- rocketmq-api-pro/src/main/java/com/monkeyzi/rocketmq/mq/demo/simple/MqConsumer.java
+++ rocketmq-api-pro/src/main/java/com/monkeyzi/rocketmq/mq/demo/simple/MqConsumer.java
@@ -2,9 +2,7 @@ package com.monkeyzi.rocketmq.mq.demo.simple;
 
 import com.monkeyzi.rocketmq.constant.MqConstant;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
-import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
-import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
+import org.apache.rocketmq.client.consumer.listener.*;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
@@ -14,19 +12,18 @@ import java.util.List;
 public class MqConsumer {
 
     public static void main(String[] args) throws MQClientException {
-        DefaultMQPushConsumer consumer=new DefaultMQPushConsumer("tran_consumer_group_name");
+        DefaultMQPushConsumer consumer=new DefaultMQPushConsumer("oneway_consumer_group_name");
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
         consumer.setNamesrvAddr(MqConstant.NAMESRV_ADDR);
-        consumer.subscribe("topic_tran","*");
-        consumer.registerMessageListener(new MessageListenerOrderly() {
+        consumer.subscribe("topic_3","*");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
-            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> list, ConsumeOrderlyContext consumeOrderlyContext) {
-                MessageExt msg=list.get(0);
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                MessageExt msg=msgs.get(0);
                 System.out.println("消费消息："+new String(msg.getBody()));
-                return ConsumeOrderlyStatus.SUCCESS;
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
-
-
         });
         consumer.start();
         System.out.println("消费者启动成功！");
